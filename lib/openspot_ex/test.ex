3a78cebd717186d1760e259dc9a52b67f44bc4d8
Tony Collen <tony@halogenlabs.com>
2019-12-17 21:30:48 -0600

    Make it work with websockets

--- lib/openspot_ex/test.ex
+++ lib/openspot_ex/test.ex
@@ -1,30 +1,79 @@
 defmodule OpenspotEx.Test do
-  @hostname "openspot.local"
-  @json_headers [{"Accept", "application/json"}, {"Content-Type", "application/json"}, {"Connection", "keep-alive"}]
+  require Logger
+  use WebSockex
 
-  def authenticate(username \\ "openspot", password \\ "openspot") do
+  @hostname "openspot.local"
+  @default_password "openspot"
+  @json_headers [
+    {"Accept", "application/json"},
+    {"Content-Type", "application/json"},
+    {"Connection", "keep-alive"}
+  ]
+
+  def start_link(_args \\ [], state \\ %{}) do
+    {:ok, jwt} = authenticate()
+
+    IO.puts("JWT: #{jwt}")
+    url = "ws://openspot.local/" <> jwt
+
+    WebSockex.start_link(url, __MODULE__, state,
+      extra_headers: [{"Sec-WebSocket-Protocol", "openspot2"}]
+    )
+  end
+
+  def init() do
+    {:ok, jwt} = authenticate()
+    url = "ws://openspot.local/" <> jwt
+
+    IO.puts("URL: #{url}")
+
+    {:ok, %{jwt: jwt}}
+  end
+
+  @impl true
+  def handle_frame({:text, msg}, state) do
+    Logger.debug(msg)
+
+    json = Jason.decode!(msg)
+    dispatch_msg(json)
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_frame({type, msg}, state) do
+    IO.puts("Received type: #{inspect(type)}, msg: #{inspect(msg)}")
+    {:ok, state}
+  end
+
+  def dispatch_msg(%{"type" => "calllog"} = msg) do
+    IO.puts("*****************************")
+    IO.puts("Calllog: #{inspect(msg)}")
+    IO.puts("*****************************")
+  end
+
+  def dispatch_msg(_msg) do
+  end
+
+  def authenticate(password \\ @default_password) do
     {:ok, response} = HTTPoison.get(api_url("gettok"))
     {:ok, %{"token" => token}} = Jason.decode(response.body)
 
-    IO.puts "Got token: #{token}"
+    digest = digest_password(password, token)
 
-    digest = digest_password("openspot", token)
+    login_payload =
+      Jason.encode!(%{
+        token: token,
+        digest: digest
+      })
 
-    IO.puts "Digest: #{digest}" 
+    {:ok, response} = HTTPoison.post(api_url("login"), login_payload, @json_headers)
 
-    login_body = Jason.encode!(%{
-      token: token,
-      digest: digest
-    })
+    {:ok, %{"jwt" => jwt}} = Jason.decode(response.body)
 
-    IO.puts "Posting login body:"
-    IO.puts login_body
+    IO.puts("Got JWT: #{jwt}")
 
-    {:ok, response} = HTTPoison.post(api_url("login"), login_body, @json_headers)
-
-    IO.puts "********"
-    IO.puts "HTTP Response code: #{response.status_code}"
-    IO.inspect response
+    {:ok, jwt}
   end
 
   defp api_url(path), do: @hostname <> "/" <> path
@@ -32,8 +81,4 @@ defmodule OpenspotEx.Test do
   def digest_password(password, token) do
     :crypto.hash(:sha256, token <> password) |> Base.encode16() |> String.downcase()
   end
-
-  defp authenticated_json_headers(token) do
-  end
-
 end
